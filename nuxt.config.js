a6199d7ff0783077d5fed5cfa0ed58508b47b8d9
zzyo96 <1977795940@qq.com>
2019-10-15 23:26:44 +0800

    安装babel eslint sass 等

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,9 @@ module.exports = {
   ** Global CSS
   */
   css: [
+    // 'element-ui/lib/theme-chalk/index.css',
+    // 'element-ui/lib/theme-chalk/reset.css',
+    // '~assets/css/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
@@ -38,6 +41,7 @@ module.exports = {
   ** Nuxt.js modules
   */
   modules: [
+    // '@nuxtjs/axios'
   ],
   /*
   ** Build configuration
