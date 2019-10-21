498823f6df60561b2e3e4df9e51eef977431f3a2
zzyo96 <1977795940@qq.com>
2019-10-21 11:28:36 +0800

    header 切换城市

---
+++ components/public/header/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-row
+    :gutter="15"
+    class="m-header">
+    <el-col :span="6">
+      <geo/>
+    </el-col>
+    <el-col :span="6">
+      <user/>
+    </el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  </el-row>
+</template>
+
+<script>
+  import Geo from './geo.vue'
+  import User from './user.vue'
+  export default {
+    name: "index.vue",
+    components:{
+        Geo,
+        User
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
